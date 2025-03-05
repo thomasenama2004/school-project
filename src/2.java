02db5841b720e3bcda68fa090d1358e87cada982
thomasenama2004 <thomasenama2004@gmx.com>
2025-03-05 13:07:41 +0000

    Add 2.java

---
+++ src/2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class RandomJavaCode {
+    public static void main(String[] args) {
+        // Generate a random number between 1 and 10
+        int randNum = (int)(Math.random() * 10 + 1);
+
+        // Print the random number
+        System.out.println("Random number: " + randNum);
+    }
+}
