a5b91e6e7ddc1af707b72ee0a0128de053041a22
thomasenama2004 <thomasenama2004@gmx.com>
2025-03-07 06:29:28 +0000

    Add 4.java

---
+++ src/4.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class RandomCode {
+    public static void main(String[] args) {
+        int randomNumber = new Random().nextInt(10);
+        System.out.println(randomNumber);
+    }
+}
